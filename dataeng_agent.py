80b45ba8d8ec2398154b9aa793b29c7e25fe4d09
hann-mbp14 <gwu.suomi@outlook.com>
2024-06-20 00:54:46 +0300

    before adding chat history funcs

--- dataeng_agent.py
+++ dataeng_agent.py
@@ -1,10 +1,14 @@
 # %% Set up the environment
 import os
+import subprocess
 import json
 import ast
 import logging
 from typing import List
 import pandas as pd
+import local_templates
+import traceback
+import sys
 
 # from langchain.llms import OpenAI
 from langchain_community.llms import OpenAI
@@ -31,100 +35,44 @@ os.environ["LANGCHAIN_PROJECT"] = "default"
 logging.basicConfig(filename="data_engineering.log", level=logging.INFO)
 
 
-"""Prompt templates definitions"""
-# Define system prompt
+"""Prompt definitions"""
 
-full_prompt = PromptTemplate.from_template(
-    """{system_prompt}
 
-    {user_prompt}
-    
-    {dataset_summary}
-    """
-)
-system_prompt = PromptTemplate.from_template(
-    """
-You are an expert in Tiny Machine Learning (TinyML), highly skilled in the workflow, tools, techniques, and best practices of TinyML operations. Your expertise extends to hardware, including microcontrollers. You will be asked questions regarding various phases, for example, data engineering, model designing, model evaluation, etc, of TinyMLOps and may need to generate code to execute corresponding tasks, for example, data cleaning, model training code, etc.
-"""
+# %%
+full_prompt = PromptTemplate.from_template(local_templates.full_prompt_template)
+system_prompt = PromptTemplate.from_template(local_templates.system_prompt_template)
+user_prompt_0 = PromptTemplate.from_template(local_templates.user_prompt_template_0)
+user_prompt_1 = PromptTemplate.from_template(local_templates.user_prompt_template_1)
+user_prompt_2 = PromptTemplate.from_template(local_templates.user_prompt_template_2)
+dataset_summary_prompt = PromptTemplate.from_template(
+    local_templates.dataset_summary_prompt_template
 )
 
-user_prompt_template_0 = """
-# OBJECTIVE #
-I want to train a model to {purpose}. Now analyze the dataset I uploaded to give practical suggestions in sequential order to do data engineering based on the inspirations you get from this dataset.
-# RESPONSE FORMAT #
-Keep the answer short and concise. Do not add a title and summary text, question or conclusion in your answer. The output format should be in JSON objects containing key-value pairs where key is operation name (all lowercase, connected by _), and value is the operation description. output should not contain any char except JSON valid chars, no line change char etc..
-"""
-
-# Define user prompt templates
-user_prompt_template_1 = """
-{{
-  "task": {{
-    "target_goal": {{
-      "{operation_n}": "{operation_n_explanation}",
-    }},
-    "task_requirements": "Write me practical code to implement the operation.",
-    "dataset_path": "{dataset_path}",
-    "processing_already_applied": [],
-    "format": "output only a code block; report any file and directory structure change caused by your output code; reply similarly to the format of JSON to specify dataset path after change"
-  }}
-}}
-"""
-
-user_prompt_template_2 = """
-{{
-  "executed_code": "{executed_code}",
-  "caused_error": "{error_info}",
-  "task": "Regenerate the last task to avoid this error."
-}}
-"""
-suggestion_json_schema = {
-    "title": "suggestion_table",
-    "description": "suggested operations to perform on the dataset",
-    "type": "object",
-    "properties": {
-        "suggestion_name_1": {
-            "type": "string",
-            "description": "first suggested operation",
-        },
-        "suggestion_name_2": {
-            "type": "string",
-            "description": "second suggested operation",
-        },
-        "suggestion_name_n": {
-            "type": "string",
-            "description": "n-th suggested operation",
-        },
-    },
-}
-dataset_summary_prompt_template = """The dataset summary information is as follows: {dataset_summary}
-"""
+# %%
 
 
-# def dataset_loader(dataset_path):
-#     loader = CSVLoader(file_path=dataset_path)
-#     return loader.load()
-
-
+# %%
+# %%
 def dataset_summary(dataset_path):
     """Return the summary of the dataset as a string to povide inspirations to LLM for data processing suggestion generation."""
     dataframe = pd.read_csv(dataset_path)
-    return str(
-        {
-            "Dataset shape": dataframe.shape,
-            "Basic Statistics": dataframe.describe(),
-        }
+    description = "columns        " + str(dataframe.describe())
+    return (
+        str(
+            {
+                "Dataset shape": dataframe.shape,
+                "Basic Statistics": description,
+            }
+        )
+        + "\nNote: column names are case sensitive, remember that."
     )
-
-
-# # %% TEst the dataset loader
-# purpose = "detect spam emails"
-# dataset_path = "./data/spam.csv"
 
 
 # %%
 def generate_suggestion_table(
     purpose: str,  # The purpose of the model input by user
     dataset_path: str,  # Path to the dataset provided by the user
+    dataset_intro: str,  # Introduction to the dataset provided by the user
 ):
     """Returns a suggestion table based on the dataset meta-info."""
     dataset_summary_str = dataset_summary(dataset_path)
@@ -133,10 +81,10 @@ def generate_suggestion_table(
         final_prompt=full_prompt,
         pipeline_prompts=[
             ("system_prompt", system_prompt),
-            ("user_prompt", PromptTemplate.from_template(user_prompt_template_0)),
+            ("user_prompt", user_prompt_0),
             (
                 "dataset_summary",
-                PromptTemplate.from_template(dataset_summary_prompt_template),
+                dataset_summary_prompt,
             ),
         ],
     )
@@ -144,10 +92,10 @@ def generate_suggestion_table(
     prompt = pipeline_prompt_0.format(
         purpose=purpose,
         dataset_summary=dataset_summary_str,
+        dataset_intro=dataset_intro,
     )
 
     returnable = llm.invoke(prompt)
-
     return json.loads(returnable.content)
 
 
@@ -158,79 +106,122 @@ def generate_suggestion_table(
 def get_user_input():
     purpose = input("The model is trained for...? ")
     dataset_path = input("Please enter the dataset path: ")
-    return purpose, dataset_path
+    dataset_intro = input("Please provide a brief introduction to the dataset: ")
+    return purpose, dataset_path, dataset_intro
 
 
 # Create a function to execute the code snippet safely
 def execute_code(code):
+    """Decoupled local code execution function."""
+
+    tmp_file = "./tmp/tmp1.py"
+    with open(tmp_file, "w") as file:
+        file.write(code)
+
+    # Try to execute the script and handle errors
     try:
-        ast.literal_eval(code)
-        return None
-    except Exception as e:
-        return str(e)
+        result = subprocess.run(["python", tmp_file], capture_output=True, text=True)
+        if result.returncode == 0:
+            return None
+        else:
+
+            # Handle the error
+            print("An error occurred during script execution.")
+            logging.error(result.stderr)
+
+            return result.stderr
+
+    finally:
+        # Ensure the script file is deleted regardless of success or failure
+        os.remove(tmp_file)
+    # try:
+    #     print(type(code))
+    #     print(code)
+    # ast.literal_eval(code)
+    #     return None
+    # except Exception as e:
+    #     err_msg = traceback.format_exc() + "\n" + str(e)
+    #     logging.error(err_msg)
+    #     return err_msg
 
 
 # Create a function to prompt the LLM for code snippets
 def prompt_llm(user_prompt):
+    """Instantiate the ChatOpenAI class and prompt the LLM for code snippets each time."""
     llm = ChatOpenAI(api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL_NAME)
 
     response = llm.invoke(user_prompt)
-    return response
+    return response.content
 
 
 # Create a function to handle retries
-def retry_code_execution(code, max_retries=2):
-    for _ in range(max_retries):
+def retry_code_execution(
+    code,
+    max_retries=2,
+    llm=None,
+):
+    """General function for local code execution, also responsible for handling re-prompting and re-executing in case of errors."""
+    for i in range(max_retries + 1):
+        if i == max_retries:
+            sys.exit(
+                f"An error could not be resolved after {max_retries} retries: \n{error}"
+            )
         error = execute_code(code)
         if not error:
             return None
         logging.error(f"Error: {error}")
-        user_prompt = user_prompt_template_2.format(
-            executed_code=code, error_info=error
-        )
-        response = prompt_llm.invoke(user_prompt)
+        user_prompt = user_prompt_2.format(executed_code=code, error_info=error)
+        response = llm.invoke(user_prompt).content
         code = response.split("```python")[1].split("```")[0]
-    return error
+
+    return [error, llm]
 
 
 """The code below belongs to main() function"""
+
+
 # %%
 # Create a main function to orchestrate the data engineering process
-# def main():
-# purpose, dataset_path = get_user_input()
-purpose = "detect spam emails"
-dataset_path = "./data/spam.csv"
-suggestion_table = generate_suggestion_table(purpose, dataset_path)
-# %%
-print(suggestion_table)
-# %%
-processing_already_applied = []
+def main():
+    # purpose, dataset_path = get_user_input()
+    purpose = "detect spam emails"
+    dataset_path = "./data/smartphone.csv"
+    dataset_intro = "This dataset was collected from the Smartphone sensors and can be used to analyse behaviour of a crowd, for example, an anomaly."
+    suggestion_table = generate_suggestion_table(purpose, dataset_path, dataset_intro)
 
-for operation_n, operation_n_explanation in suggestion_table.items():
-    user_prompt = user_prompt_template_1.format(
-        operation_n=operation_n,
-        operation_n_explanation=operation_n_explanation,
-        dataset_path=dataset_path,
-    )
-    response = prompt_llm(user_prompt)
+    # %%
+    processing_already_applied = []
+    llm = ChatOpenAI(api_key=OPENAI_API_KEY, model_name=OPENAI_MODEL_NAME)
 
-    code_snippet = response.split("```python")[1].split("```")[0]
-    error = retry_code_execution(code_snippet)
+    for operation_n, operation_n_explanation in suggestion_table.items():
+        user_prompt = user_prompt_1.format(
+            operation_n=operation_n,
+            operation_n_explanation=operation_n_explanation,
+            dataset_path=dataset_path,
+            list_processing_already_applied=str(processing_already_applied),
+        )
+        response = llm.invoke(user_prompt).content
 
-    if error:
-        logging.error(f"Error: {error}")
-        break
+        code_snippet = response.split("```python")[1].split("```")[0]
+        returned_list = retry_code_execution(code_snippet, 2, llm)
+        if type(returned_list) is List:
+            error = returned_list[0]
+            llm = returned_list[1]
+            logging.error(f"Error: {error}")
+            break
 
-    processing_already_applied.append(operation_n)
-    dataset_path = json.loads(response.split("```json")[1].split("```")[0])[
-        "dataset_path"
-    ]
+        processing_already_applied.append(operation_n)
+        dataset_path = json.loads(
+            response.split("```json")[1].split("```")[0].strip().replace("'", '"')
+        )
 
-logging.info("Data engineering process completed.")
+    logging.info("Data engineering process completed.")
+
+
 """The code above belongs to main() function"""
 
 
 # %%
 # Run the main function
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
---
+++ local_templates.py
@@ -0,0 +1,42 @@
+full_prompt_template = """{system_prompt}
+
+{user_prompt}
+
+{dataset_summary}"""
+
+system_prompt_template = """ You are an expert in Tiny Machine Learning (TinyML), highly skilled in the workflow, tools, techniques, and best practices of TinyML operations. Your expertise extends to hardware, including microcontrollers. You will be asked questions regarding various phases, for example, data engineering, model designing, model evaluation, etc, of TinyMLOps and may need to generate code to execute corresponding tasks, for example, data cleaning, model training code, etc.
+"""
+
+
+user_prompt_template_0 = """# OBJECTIVE #
+I want to train a model to {purpose}. Now analyze the dataset I uploaded to give practical suggestions in sequential order to do data engineering based on the inspirations you get from this dataset. When applying each suggestion, the dataset would be read from the file again, so do not give suggestions like "load_dataset", because that is basic operation contained in every suggestion.
+
+# RESPONSE FORMAT #
+# Keep the answer short and concise. Do not add a title and summary text, question or conclusion in your answer. The output format should be in JSON objects containing key-value pairs where key is operation name (all lowercase, connected by _), and value is the operation description. output should not contain any char except JSON valid chars, no line change char etc.."""
+
+user_prompt_template_1 = """
+{{
+  "task": {{
+    "target_goal": {{
+      "{operation_n}": "{operation_n_explanation}",
+    }},
+    "task_requirements": "Write me practical code to implement the operation.",
+    "dataset_path": {dataset_path},
+    "processing_already_applied": {list_processing_already_applied},
+    "format": "output only two code blocks; code in the first block; in the second code block, report any file and directory structure change caused by your output code; replay similarly to the format of JSON to specify dataset path after change",
+  }}
+}}
+"""
+
+user_prompt_template_2 = """{{
+    "executed_code": "{executed_code}",
+    "caused_error": "{error_info}",
+    "task": "Regenerate the last task to avoid this error.",
+    "format": "output only two code blocks; code in the first block; in the second code block, report any file and directory structure change caused by your output code; replay similarly to the format of JSON to specify dataset path after change",
+}}"""
+
+
+dataset_summary_prompt_template = """
+    Brief introduction to the dataset: {dataset_intro}\n
+    The dataset summary information is as follows:\n {dataset_summary}
+    """
